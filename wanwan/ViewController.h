5edeedbcdf6cc7f69b68b7ed64aee9bf4cd26a68
wangyun-hero <wangyun120411@163.com>
2017-08-27 22:01:33 +0800

    diyici

---
+++ wanwan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wanwan
+//
+//  Created by 王云 on 2017/8/27.
+//  Copyright © 2017年 王云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ wanwan/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  wanwan
+//
+//  Created by 王云 on 2017/8/27.
+//  Copyright © 2017年 王云. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
